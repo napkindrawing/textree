22ff6a6eeeb8f21426f3f0158002144908a92a47
Mike Walker <mike.walker@napkindrawing.com>
2011-06-09 18:04:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textree::Application.config.secret_token = 'a3686865e85a38cb85b3ff2e22fc557a9716838b2d777ca4f2ac7be6e5d6ae5b324d886358564aae1c035eaeadc17f4966b214d905bcb3be3a9b17d3105c7f04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Textree::Application.config.session_store :cookie_store, :key => '_textree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Textree::Application.config.session_store :active_record_store

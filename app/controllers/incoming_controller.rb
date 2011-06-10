c203db56b27ffa5c13270943b6f80dbf2d662a8e
Mike Walker <mike.walker@napkindrawing.com>
2011-06-10 16:23:37 +0000

    use name of member if member sent message

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,13 +4,16 @@ class IncomingController < ApplicationController
     def index
         account = Twilio::RestAccount.new(Rails.application.config.TWILIO_SID, Rails.application.config.TWILIO_TOKEN)
         members = Member.all
+        from_member = members.find { |m| '1'+m.phone_number.gsub(/\D/,'') == params[:From].gsub(/\D/,'') }
+        STDERR.puts "from member: #{from_member}"
+        from = from_member ? from_member.name : params[:From]
         members.each do |member| 
             # Avert infinite loops!
             return if member.phone_number == Rails.application.config.TWILIO_NUMBER
             d = {
                 'From' => Rails.application.config.TWILIO_NUMBER,
                 'To' => member.phone_number,
-                'Body' => '['+params[:From]+']: '+params[:Body]
+                'Body' => '['+from+']: '+params[:Body]
             }
             STDERR.puts "text going out: #{d.inspect}"
             resp = account.request(
